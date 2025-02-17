1ab999edff8df49a27d81079e4977c1315cdd7fc
Livia Gomes <liviag97@gmail.com>
2025-02-17 16:33:42 -0300

    error dialog created

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { catchError } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -16,9 +19,25 @@ export class CoursesComponent implements OnInit {
 
   // Estamos passando o CoursesService via construtor para que seja injetado automaticamente
   // (classes de serviço são sempre marcadas como @Injectable pelo Angular)
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => { // em caso de erro no método
+        this.onError('Erro ao carregar cursos.');
+        return of([]); // retornar um observable que retorna um array vazio
+      })
+    );
   }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
 
   ngOnInit(): void {
   }
